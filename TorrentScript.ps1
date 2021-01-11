fa8097084e1e436596e62b83516def5bc996ebd1
Rouzax <GitHub@mgdn.nl>
2021-01-11 10:01:16 +0100

    wrongfully set TotalSubsToExtract to 0 for each ep

--- TorrentScript.ps1
+++ TorrentScript.ps1
@@ -444,8 +444,6 @@ function Start-MKV-Subtitle-Strip {
         $SubIDsToRemove = @()
         $SubsToExtract = @()
         $SubNamesToKeep = @()
-        $TotalSubsToExtract = 0
-        $TotalSubsToRemove = 0
 
         $episode.FileTracks | ForEach-Object {
             $FileTrack = $_
