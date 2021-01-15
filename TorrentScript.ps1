f6be4d87a5d5a262c08f94972e7bef83ea3cb992
Rouzax <GitHub@mgdn.nl>
2021-01-15 14:54:07 +0100

    SubtitleEdit changes

--- TorrentScript.ps1
+++ TorrentScript.ps1
@@ -608,7 +608,7 @@ function Start-SubEdit {
     $StartInfo.RedirectStandardError = $true
     $StartInfo.RedirectStandardOutput = $true
     $StartInfo.UseShellExecute = $false
-    $StartInfo.Arguments = @("/convert", "$Files", "subrip", "/inputfolder`:`"$Source`"", "/overwrite", "/fixcommonerrors", "/removetextforhi", "/fixcommonerrors")
+    $StartInfo.Arguments = @("/convert", "$Files", "subrip", "/inputfolder`:`"$Source`"", "/overwrite", "/fixcommonerrors", "/removetextforhi", "/fixcommonerrors", "/fixcommonerrors")
     $Process = New-Object System.Diagnostics.Process
     $Process.StartInfo = $StartInfo
     $Process.Start() | Out-Null
